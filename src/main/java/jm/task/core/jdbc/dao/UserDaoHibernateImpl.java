4344bb28ead2cf0262ed99d32fe783fded98a42d
Artem <azaozerov@gmail.com>
2023-01-09 16:10:25 +0500

    version 0.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,13 +4,13 @@ import jm.task.core.jdbc.util.*;
 import jm.task.core.jdbc.model.*;
 import org.hibernate.Session;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
     }
-
 
     @Override
     public void createUsersTable() {
@@ -21,55 +21,81 @@ public class UserDaoHibernateImpl implements UserDao {
                                       last_name   varchar(100) NOT NULL,
                                       age         tinyint NOT NULL)
                                 """;
-        runNativeQuery(createTableSQL);
+        Session session = Util.getMySQLSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(createTableSQL).executeUpdate();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
         String dropTableSQL = "DROP TABLE IF EXISTS my_users_h";
-        runNativeQuery(dropTableSQL);
+        Session session = Util.getMySQLSessionFactory().openSession();
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(dropTableSQL).executeUpdate();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User newUser = new User(name, lastName, age);
         Session session = Util.getMySQLSessionFactory().openSession();
-        session.beginTransaction();
-        session.save(newUser);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(newUser);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        User deleteUser = new User();
-        deleteUser.setId(id);
+        String deleteUserSQL = "DELETE FROM my_users_h WHERE id = :id";
         Session session = Util.getMySQLSessionFactory().openSession();
-        session.beginTransaction();
-        session.delete(deleteUser);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(deleteUserSQL).setParameter("id", id).executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
         String getAllUsersSQL = "SELECT * FROM my_users_h";
         Session session = Util.getMySQLSessionFactory().openSession();
-        List<User> usersList = session.createNativeQuery(getAllUsersSQL).addEntity(User.class).list();
-        session.close();
-        return usersList;
+        try {
+            List<User> usersList = session.createNativeQuery(getAllUsersSQL).addEntity(User.class).list();
+            return usersList;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
         String clearTableSQL = "TRUNCATE TABLE my_users_h";
-        runNativeQuery(clearTableSQL);
-    }
-    private void runNativeQuery(String query) {
         Session session = Util.getMySQLSessionFactory().openSession();
-        session.beginTransaction();
-        session.createNativeQuery(query).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.createNativeQuery(clearTableSQL).executeUpdate();
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
     }
 }
